49b1040e477af11dd502a6c5f05dfcccb3043149
albert22o <124139885+albert22o@users.noreply.github.com>
2023-03-15 09:33:30 +0700

    Изменил местоположение кода и убрал учет регистра

--- main.c
+++ main.c
@@ -6,57 +6,56 @@
 
 int main()
 {
+    int ind_open_bracket = 0, ind_close_bracket = 0, ind_last_num_elm = 0,
+        ind_first_num_elm = 0, ind_second_num_elm = 0, error = 0;
+    // Open file, if couldn't - output mistake
     FILE* file1;
-    FILE* file;
-
-    file1 = fopen("test.txt", "r");
-
-    if (!file1) { 
-        printf("Error: cannot open file. Check name of file\n");
+    file1 = fopen("Input.txt", "r");
+    if (!file1) {
+        printf("Error: cannot open file. Check name of file \n");
+        return 1;
     }
 
-    int i, ind_open_bracket = 0, ind_close_bracket = 0, ind_last_num_elm = 0,
-           ind_first_num_elm = 0, ind_second_num_elm = 0;
-    int length = 0, count = 0, element = 0, error = 0;
-
-    while (1) {
+    int length = 0, element = 0;
+    while (true) {
         element = fgetc(file1);
-        if (element == EOF) {
-            if (feof(file1) != 0) {
+        if (element == EOF)
+            if (feof(file1) != 0)
                 break;
-            }
-        }
-        count++;
+        length++;
     }
-    length = count;
+
     fclose(file1);
 
-    char a[length], b[6] = "circle";
-    file = fopen("test.txt", "r");
+    char b[] = "circle";
+    char c[] = "CIRCLE";
+    char* a;
+    a = (char*)(malloc(length * sizeof(char)));
+    FILE* file;
+    file = fopen("Input.txt", "r");
+
+    if (length == 0)
+        return 1;
+
+    int i;
     while (fgets(a, length + 1, file)) {
         printf("%s", a);
 
-        // check 'circle and finding index of '(' symbol
         for (i = 0; i < 7; i++) {
-            if (a[i] != b[i] && i < 6) {
+            if (a[i] != b[i] && a[i] != c[i] && i < 6) {
                 printf("Error at column %d: expected 'circle'\n", i);
                 error = 1;
                 break;
-                ;
             }
             ind_open_bracket = i;
         }
-        // printf("'(' = %d\n", ind_open_bracket);
 
         for (i = 0; i < length; i++) {
-            if (a[i] == ')') {
+            if (a[i] == ')')
                 ind_close_bracket = i;
-            } else {
+            else
                 ind_close_bracket = length - 1;
-            }
         }
-
-        // check first number
         for (i = ind_open_bracket + 1; a[i] != ' '; i++) {
             if (error == 0) {
                 if (a[i] == ',') {
@@ -72,13 +71,10 @@ int main()
                     break;
                 }
                 ind_first_num_elm = i;
-            } else {
+            } else
                 break;
-            }
         }
-        // printf("index of first numer = %d\n", ind_first_num_elm);
 
-        // check second number
         for (i = ind_first_num_elm + 2; a[i] != ','; i++) {
             if (error == 0) {
                 if (a[i] == ')') {
@@ -93,48 +89,36 @@ int main()
                     break;
                 }
                 ind_second_num_elm = i;
-            } else {
+            } else
                 break;
-            }
         }
-        // printf("index of second number = %d\n", ind_second_num_elm);
 
-        // check last number
         for (i = ind_second_num_elm + 3; i < ind_close_bracket; i++) {
             if (error == 0) {
                 if (isdigit(a[i]) == 0 && a[i] != '.') {
-                    if (a[i] == ')' || a[i] == '(' || a[i] == ' ') {
+                    if (a[i] == ')' || a[i] == '(' || a[i] == ' ')
                         break;
-                    }
                     error = 1;
                     printf("Error at column %d: expected '<double>'\n", i);
                     break;
                 }
                 ind_last_num_elm = i;
-            } else {
+            } else
                 break;
-            }
         }
-        // printf("index of last number = %d\n", ind_last_num_elm);
 
-        // check ')' symbol
         for (i = ind_last_num_elm + 1; i < length; i++) {
             if (error == 0) {
                 if (a[i] != ')') {
                     error = 1;
                     printf("Error at column %d: expected ')'\n", i);
-                    break;
-                } else {
+                } else
                     ind_close_bracket = i;
-                    break;
-                }
-            } else {
                 break;
-            }
+            } else
+                break;
         }
-        // printf("')' = %d\n", ind_close_bracket);
 
-        // check unexpected tokens
         for (i = ind_close_bracket + 1; i < length; i++) {
             if (error == 0) {
                 if (a[i] == '\n') {
@@ -151,10 +135,8 @@ int main()
             }
         }
 
-        if (error == 0) {
+        if (error == 0)
             printf("No Errors!\n");
-        }
-
         error = 0;
         printf("\n");
     }
